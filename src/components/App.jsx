6013d709e6d1c9556592ff786c5ce315d13bbad7
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-02-24 02:48:51 +0200

    add normalize and proptypes modules

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
-export const App = () => {
-  return <div>React homework template</div>;
-};
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <div>React homework template</div>;
+  }
+}
+
+export default App;
