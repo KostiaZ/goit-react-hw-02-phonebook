9916a582d57902eb707792b7627e76b94a89c162
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-02-25 23:55:59 +0200

    add filter function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
   addContact = ({ name, number }) => {
@@ -57,20 +52,17 @@ class App extends Component {
     }));
   };
   render() {
-    const { filter } = this.state.contacts;
-    // const visibleContacts = this.getVisibleContacts;
     return (
       <div>
         <h1>Phonebook</h1>
         <ContactForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
-        <Filter value={filter} onChange={this.changeFilter} />
+        <h3>All contacts: {this.state.contacts.length}</h3>
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
         <ContactList
-          contacts={this.state.contacts}
+          contacts={this.getVisibleContacts()}
           onDeleteContact={this.deleteContact}
         />
-        {/* // contacts={visibleContacts}
-        //  */}
       </div>
     );
   }
